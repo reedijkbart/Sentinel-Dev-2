2bf1298ec67844478bfd49d0168b452f5c41e32b
Bart Reedijk <91743283+reedijkbart@users.noreply.github.com>
2022-11-10 13:53:36 +0100

    Update note.ps1

--- .script/note.ps1
+++ .script/note.ps1
@@ -2,7 +2,7 @@ Set-PSRepository PSGallery -InstallationPolicy Trusted
 Install-Module PowerShell-yaml
 #Import-Module .script/ConvertAnalyticsRuleFromYamlToArm.ps1
 
-$newDirectory = New-Item -ItemType Directory -Path "Detections/SigninLogs"
+$newDirectory = New-Item -ItemType Directory -Path "Detections/SigninLogs/dir2"
 foreach($parserYaml in $(Get-Item ***/*.yaml))
 {
      $outputFilePath = "$($newDirectory.FullName)/$($parserYaml.BaseName).json"
