9160754de5d7bc04de77163124b5dffd1b7c7f2a
Bart Reedijk <91743283+reedijkbart@users.noreply.github.com>
2022-11-16 13:26:33 +0100

    Update note.ps1

--- .script/note.ps1
+++ .script/note.ps1
@@ -8,3 +8,18 @@ foreach($parserYaml in $(Get-Item ***/*.yaml))
      $outputFilePath = "$($newDirectory.FullName)/$($parserYaml.BaseName).json"
      ./ConvertAnalyticsRuleFromYamlToArm $parserYaml.FullName $outputFilePath
 }
+
+
+Install-Module PowerShell-yaml
+Import-Module .\ConvertAnalyticsRuleFromYamlToArm.ps1
+
+$Homepath = "Detections"
+$Outpath  = "Detections/JSON"
+
+if (!(Test-Path -Path $Outpath -PathType Container)){ New-Item -ItemType Directory -Path $Outpath}
+
+foreach($parserYaml in $(Get-Childitem -path $Homepath -Recurse -Include *.yaml))
+{
+     $outputFilePath = "$Outpath\$($parserYaml.BaseName).json"
+     ConvertAnalyticsRuleFromYamlToArm $parserYaml.FullName $outputFilePath
+}
